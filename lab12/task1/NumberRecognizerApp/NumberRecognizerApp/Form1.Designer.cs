e6a56c4386b9a2fcdf5029d86cd788277ebf5bff
NazarFrazovir <148887298+NazarFrazovir@users.noreply.github.com>
2025-05-08 00:42:30 +0300

    Add files via upload

---
+++ lab12/task1/NumberRecognizerApp/NumberRecognizerApp/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace NumberRecognizerApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(120, 30);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "Завантажити файл";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(150, 12);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(120, 30);
+            this.btnProcess.TabIndex = 1;
+            this.btnProcess.Text = "Обробити";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 60);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(480, 100);
+            this.txtInput.TabIndex = 2;
+
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 180);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(480, 100);
+            this.txtOutput.TabIndex = 3;
+
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 300);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(480, 150);
+            this.txtLog.TabIndex = 4;
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(504, 470);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnLoadFile);
+            this.Name = "Form1";
+            this.Text = "Розпізнавання чисел з тексту";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
---
+++ lab12/task1/NumberRecognizerApp/NumberRecognizerApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.Number;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace NumberRecognizerApp
+{
+    public partial class Form1 : Form
+    {
+        string inputFilePath = "";
+        string outputFilePath = "output.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                inputFilePath = ofd.FileName;
+                txtInput.Text = File.ReadAllText(inputFilePath);
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                MessageBox.Show("Спочатку завантажте текст.");
+                return;
+            }
+
+            string inputText = txtInput.Text;
+            var results = NumberRecognizer.RecognizeNumber(inputText, Culture.English);
+            string modifiedText = inputText;
+            string resultLog = "";
+
+            foreach (var result in results)
+            {
+                string originalText = result.Text;
+                string resolvedValue = result.Resolution["value"].ToString();
+                int start = result.Start;
+                int end = result.End;
+
+
+                resultLog += $"Розпізнаний текст (число): {originalText}\r\n";
+                resultLog += $"Початковий індекс у рядку: {start}\r\n";
+                resultLog += $"Кінцевий індекс у рядку: {end}\r\n";
+                resultLog += $"Розпізнане значення числа: {resolvedValue}\r\n\r\n";
+
+                // Замінити словесне число на цифру
+                modifiedText = modifiedText.Replace(originalText, resolvedValue);
+            }
+
+            txtOutput.Text = modifiedText;
+            txtLog.Text = resultLog;
+
+            File.WriteAllText(outputFilePath, modifiedText);
+            MessageBox.Show("Результат збережено у 'output.txt'");
+        }
+    }
+}
